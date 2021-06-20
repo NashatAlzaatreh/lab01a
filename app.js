f9983194f3694377d3cdef1077c7bb622b028a48
NashatAlzaatreh <nashat.alzaatra@yahoo.com>
2021-06-20 16:28:15 +0300

    lab01

---
+++ app.js
@@ -0,0 +1,19 @@
+let q1 
+q1=prompt("what is your name?");
+console.log(q1);
+alert("wlcome "+ q1)
+
+let q2
+q2=prompt("Are you an engineer?")
+console.log(q2);
+alert(q2+"! You are a lucky person, " + q1 )
+
+let q3 
+q3=prompt("Who is your favorite football team?");
+console.log(q3);
+alert(q3+ "!!! its a bad team")
+
+let q4
+q4=prompt("why you visit this web page?");
+console.log(q4);
+alert(q4 + " ! nice to meet you")
